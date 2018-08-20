238e0b80f6aa0256defc24d818a4e907d48733a9
perison <perisonchan@gmail.com>
2018-08-20 22:08:26 +0800

    database setting

--- XBackend/settings.py
+++ XBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!o)-^)0t5!6b84%8k9!sot&t#=a%(8wh&&t1r*ciutsql^d4lh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'interface',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'XBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'xprojectDB',
+        'USER': 'perison',
+        'PASSWORD': '123!@#',
+        'HOST': 'localhost',
+        'PORT': '6667',
     }
 }
 
--- interface/models.py
+++ interface/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Wagers(models.Model):
+    id = models.IntegerField
+    wager = models.CharField(max_length=5)
+
 # Create your models here.
